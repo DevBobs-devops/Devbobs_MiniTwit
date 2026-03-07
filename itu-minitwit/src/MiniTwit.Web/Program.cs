e4ed594109291a5960bad904037dcff086d29dc1
Marius Holm <marho@itu.dk>
2026-03-07 10:36:12 +0100

    feat: expose /metrics
    Co-authored-by: Laura <llsa@itu.dk>
    
    Inspiration: https://oneuptime.com/blog/post/2026-01-25-prometheus-metrics-dotnet/view

--- itu-minitwit/src/MiniTwit.Web/Program.cs
+++ itu-minitwit/src/MiniTwit.Web/Program.cs
@@ -4,6 +4,7 @@ using Chirp.Infrastructure.Chirp.Services;
 using Chirp.Infrastructure.data;
 using Chirp.Web;
 using Microsoft.EntityFrameworkCore;
+using Prometheus;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -32,6 +33,14 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseMetricServer();
+
+// Collect HTTP request metrics automatically
+app.UseHttpMetrics(options =>
+{
+    options.AddCustomLabel("service", context => "my-api");
+});
 
 if (app.Environment.IsDevelopment())
 {
@@ -62,6 +71,8 @@ else
         DbInitializer.SeedDatabase(context);
     }    
 }
+
+
 
 
 
