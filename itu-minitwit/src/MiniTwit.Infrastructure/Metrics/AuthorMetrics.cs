4a7f4bb2a5460b83ef0dee921aa0e313b548feaf
Marius Holm <marho@itu.dk>
2026-03-08 12:02:26 +0100

    feat: added counter for total authors created

---
+++ itu-minitwit/src/MiniTwit.Infrastructure/Metrics/AuthorMetrics.cs
@@ -0,0 +1,16 @@
+﻿using Prometheus;
+
+namespace Chirp.Infrastructure.Metrics;
+
+public class AuthorMetrics
+{
+    private static readonly Counter TotalAuthors = Prometheus.Metrics.CreateCounter(
+        "total_users",
+        "Total number of users");
+    
+    public void IncrementAuthors()
+    {
+        TotalAuthors.Inc();
+    }
+
+}
