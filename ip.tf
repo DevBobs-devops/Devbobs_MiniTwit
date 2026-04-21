9c9812ddb5fd974bd0baa2540fa383abe38b2416
Marius Holm <marho@itu.dk>
2026-04-21 12:18:40 +0200

    chore: add link for digitalocean_floating_ip_assignment

--- ip.tf
+++ ip.tf
@@ -3,6 +3,7 @@
 #  region = var.region
 #}
 
+#https://docs.digitalocean.com/reference/terraform/reference/resources/floating_ip_assignment/
 resource "digitalocean_floating_ip_assignment" "public-ip" {
   ip_address = 134.199.190.79
   droplet_id = digitalocean_droplet.minitwit-swarm-leader.id
