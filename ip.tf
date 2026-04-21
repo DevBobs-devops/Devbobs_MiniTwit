8c847c3e6d0c1ca7afb20508a094e60e03847d1e
Marius Holm <marho@itu.dk>
2026-04-21 12:08:54 +0200

    feat: hardcode out floating ip
    Co-authored-by: Jonas jones@itu.dk
    Co-authored-by: Laura llsa@itu.dk

--- ip.tf
+++ ip.tf
@@ -1,7 +1,12 @@
-#resource "digitalocean_floating_ip_assignment" "public-ip" {
-#  ip_address = digitalocean_floating_ip.public-ip.ip_address
-#  droplet_id = digitalocean_droplet.minitwit-swarm-leader.id
+# https://developer.hashicorp.com/terraform/language/syntax/configuration
+#resource "digitalocean_floating_ip" "public-ip" {
+#  region = var.region
 #}
+
+resource "digitalocean_floating_ip_assignment" "public-ip" {
+  ip_address = 134.199.190.79
+  droplet_id = digitalocean_droplet.minitwit-swarm-leader.id
+}
 
 output "public_ip" {
   value = 134.199.190.79
