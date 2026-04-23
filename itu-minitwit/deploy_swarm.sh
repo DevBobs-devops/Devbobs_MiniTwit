5a733da9f44961f7e6bffdfd6a5194bfa25b3ade
Marius Holm <marho@itu.dk>
2026-04-23 21:00:59 +0200

    fix: path to local stack file

--- itu-minitwit/deploy_swarm.sh
+++ itu-minitwit/deploy_swarm.sh
@@ -7,5 +7,5 @@ if [[ $1 == "build" ]]; then
     docker compose build
 fi
 
-docker stack deploy minitwit -c ./local_stack/minitwit_stack.yml --detach=false 
+docker stack deploy minitwit -c ./infrastructure/docker_swarm/local_stack/minitwit_stack.yml --detach=false 
 echo "go to: http://127.0.0.1:8080"
